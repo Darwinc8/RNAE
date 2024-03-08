bd053928b60db9d9d46325524dacadc76f79de38
Darwinc <darwincspace@gmail.com>
2024-03-08 15:02:16 -0600

    ident

--- rnae/settings.py
+++ rnae/settings.py
@@ -136,15 +136,6 @@ STATICFILES_DIRS = [
     # Otras rutas de archivos estáticos si las tienes
 ]
 
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 LOGIN_URL = '/iniciar_sesion'
 
